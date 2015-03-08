261e7bb652615a9935d4cc1919288e9e2d0abe40
maltir <007tintin@gmail.com>
2015-03-08 14:11:38 -0400

    permet au transporteur de ramasser l'énergie sur le sol

--- transport.js
+++ transport.js
@@ -3,12 +3,18 @@ module.exports = function(creep){
     Game.spawns.Spawn1.createCreep([Game.MOVE,Game.MOVE,Game.MOVE,Game.MOVE,Game.CARRY],null,{role: 'transport'});
   }else{
     if(creep.energy < creep.energyCapacity){
-      var target = creep.pos.findClosest(Game.MY_CREEPS, {
-        filter: function(object) {
-          return object.memory.role === "harvester";
-        }
-      });
-      creep.moveTo(target);
+      var energy = creep.pos.findClosest(creep.pos.findInRange(Game.DROPPED_ENERGY, 10));
+      if(energy){
+        creep.moveTo(energy);
+        creep.pickup(energy);
+      }else{
+        var target = creep.pos.findClosest(Game.MY_CREEPS, {
+          filter: function(object) {
+            return object.memory.role === "harvester";
+          }
+        });
+        creep.moveTo(target);
+      }
     }else{
       creep.moveTo(Game.spawns.Spawn1);
       creep.transferEnergy(Game.spawns.Spawn1);
