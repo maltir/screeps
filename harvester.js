242b6114d995a7ad0f87125dd61104ae0407bb64
maltir <007tintin@gmail.com>
2015-03-08 13:53:39 -0400

    correctif du transfert d'énergie v3

--- harvester.js
+++ harvester.js
@@ -1,7 +1,7 @@
 module.exports = function(creep){
   if(creep===null){
       Game.spawns.Spawn1.createCreep([Game.WORK,Game.WORK,Game.WORK,Game.MOVE,Game.CARRY],null,{role: 'harvester'});
-  }else if(creep.energy < creep.energyCapacity) {
+  }else if(creep.energy <= creep.energyCapacity) {
   	  var sources = Game.spawns.Spawn1.pos.findClosest(Game.SOURCES_ACTIVE, {ignoreCreeps: true});
   		creep.moveTo(sources);
   		creep.harvest(sources);
